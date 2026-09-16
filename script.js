61bdac1a4b151c7e9852d0d7541b2b88c2700ce3
Miaya <myfirefly22@gmail.com>
2026-09-16 00:35:48 -0400

    added to css and index

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 let prev = document.getElementById("prevAnimal");
 let nextButton = document.getElementById("nextAnimal"); 
 let join = document.getElementById("join");
+let speciesName = document.getElementById("species-name-1");
+let species1 = document.getElementById("species-1");
+let speciesImage = document.getElementById("imageSpecies")
+let species = [];
+let index = 0;
 
-let species;
-let index= 0;
+function speciesInfo() {
+	let specie = species[index];
+	
+	species1.textContent = species[index]["Conservation Status"];
+	speciesName.textContent = species[index]["Common Name"];
+}
 
+// update DOM
 async function changeSpecies()  {
   let response = await fetch ("https://student-data-api.miayadennis.workers.dev/api/v1/datasets/endangered-species-of-canada/records?limit=10");
   let data = await response.json();
-  let species = data.records;
+  species = data.records;
+  // DOM update
+  speciesInfo()
+  
+  species1.textContent = species[index]["Conservation Status"];
+	speciesName.textContent = species[index]["Common Name"];
+  
 
   console.log("species");
 }
+
 prev.addEventListener("click", function () {
 	changeSpecies();
 	
-	
 });
+
 nextButton.addEventListener("click", function () {
-	index = data.record[index] + 1;
+	index = index + 1;
 	changeSpecies();
-	
-	
-	
 });
+
 	
 	
